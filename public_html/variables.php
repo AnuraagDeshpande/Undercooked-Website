896d19657e069f81828a0918b8346a393c0f0000
tpdkrt <tpodko@gmail.com>
2024-10-17 15:34:10 +0200

    variables for server use slighlty changed

--- public_html/variables.php
+++ public_html/variables.php
@@ -1,7 +1,16 @@
 <?php
+/*
+//This is for me to test on my local machine for working
 $host = 'localhost';
 $dbname = 'test';
 $username = 'root';        
 $password = ''; 
+$socket = '/opt/lampp/var/mysql/mysql.sock';
+*/
+
+$host = 'localhost';
+$dbname = 'pyuri_db';
+$username = 'pyuri';        
+$password = 'bVjRRC'; 
 $socket = '/opt/lampp/var/mysql/mysql.sock'; 
 ?>
