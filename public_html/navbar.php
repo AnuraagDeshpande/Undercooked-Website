f5578aa96b670857f7b84a8a9fa5de8cd200e45c
Alen <akostov@constructor.university>
2024-10-17 15:19:08 +0200

    Added navbar.php file (currently not used) can be added to php files later to have a navbar in them without coding it manually

---
+++ public_html/navbar.php
@@ -0,0 +1,30 @@
+<nav class="navbar">
+        <div class="navbar_container">
+            <img src="images/logo3.png" alt="logo" class="navbar_item">
+            <a href="/" id="navbar_logo" class="logo"><h1>Undercooked</h1></a>
+            <div class="navbar_toggle" id="mobile_menu">
+                <span class="bar"></span><span class="bar"></span>
+                <span class="bar"></span>
+            </div>
+            <ul class="navbar_menu">
+                <li class="navbar_item">
+                    <a href="index.html" class="navbar_links">Home</a>
+                </li>
+                <li class="navbar_item">
+                    <a href="/" class="navbar_links">Dishes</a>
+                </li>
+                <li class="navbar_item">
+                    <a href="/" class="navbar_links">Reviews</a>
+                </li>
+                <li class="navbar_item">
+                    <a href="/" class="navbar_links">This week</a>
+                </li>
+                <li class="navbar_item">
+                    <a href="maintenance.html" class="navbar_links">Maintenance</a>
+                  </li>
+                <li class="navbar_btn">
+                    <a href="/" class="button">Login/Sign up</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
