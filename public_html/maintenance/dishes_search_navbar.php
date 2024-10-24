6b234ecc6b9a04b94cfae6f1a773469d6c4fb581
Anuraag <andeshpand@constructor.university>
2024-10-24 15:00:16 +0200

    added styles to dishes_search_navbar

--- public_html/maintenance/dishes_search_navbar.php
+++ public_html/maintenance/dishes_search_navbar.php
@@ -21,6 +21,8 @@
             background-color: #EEE;
         }
     </style>
+    <link href="styles.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 </head>
 <body>
     <?php include '../navbar.php';?>
