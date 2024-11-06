84f6911f4e6d03bc3b88dd331ee93f3a54984d95
tpdkrt <tpodko@gmail.com>
2024-11-06 17:43:04 +0100

    maintenance.php

--- public_html/maintenance/maintenance.php
+++ public_html/maintenance/maintenance.php
@@ -5,7 +5,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Undercooked Website</title>
-        <link href="<?php echo $html_root?>styles.css" rel="stylesheet"/>
+        <link href="<?php echo $html_root?>/styles.css" rel="stylesheet"/>
     </head>
     <?php
         include $_SERVER['DOCUMENT_ROOT'] . '/navbar.php';
@@ -22,21 +22,21 @@
         </p>
         <h3>Input pages for admin:</h3>
             <ul>
-                <li><a href="<?php echo $html_root?>maintenance/add_dish.php">Add a dish</a></li>
-                <li><a href="<?php echo $html_root?>maintenance/signup.php">Sign up</a></li>
-                <li><a href="<?php echo $html_root?>maintenance/rate.php">Rate dish</a></li>
-                <li><a href="<?php echo $html_root?>maintenance/goesWith.php">Add two dishes that go well with each other</a></li>
-                <li><a href="<?php echo $html_root?>maintenance/addReviews.php">Add a review</a></li>
+                <li><a href="<?php echo $html_root?>/maintenance/add_dish.php">Add a dish</a></li>
+                <li><a href="<?php echo $html_root?>/maintenance/signup.php">Sign up</a></li>
+                <li><a href="<?php echo $html_root?>/maintenance/rate.php">Rate dish</a></li>
+                <li><a href="<?php echo $html_root?>/maintenance/goesWith.php">Add two dishes that go well with each other</a></li>
+                <li><a href="<?php echo $html_root?>/maintenance/addReviews.php">Add a review</a></li>
             </ul>
         <h3>View pages for admin:</h3>
         <ul>
-            <li><a href="<?php echo $html_root?>maintenance/dishes.php">All menu</a></li>
-            <li><a href="<?php echo $html_root?>maintenance/users.php">Users</a></li>
-            <li><a href="<?php echo $html_root?>maintenance/ratingsData.php">Ratings</a></li>
-            <li><a href="<?php echo $html_root?>maintenance/goesWithData.php">Goes with data</a></li>
-            <li><a href="<?php echo $html_root?>maintenance/reviewData.php">Reviews</a></li>
-            <li><a href="<?php echo $html_root?>maintenance/search.php">Drink Search</a></li>
-            <li><a href="../dishes_queries/dishes_page.php">Dishes search using the topbar</a></li>
+            <li><a href="<?php echo $html_root?>/maintenance/dishes.php">All menu</a></li>
+            <li><a href="<?php echo $html_root?>/maintenance/users.php">Users</a></li>
+            <li><a href="<?php echo $html_root?>/maintenance/ratingsData.php">Ratings</a></li>
+            <li><a href="<?php echo $html_root?>/maintenance/goesWithData.php">Goes with data</a></li>
+            <li><a href="<?php echo $html_root?>/maintenance/reviewData.php">Reviews</a></li>
+            <li><a href="<?php echo $html_root?>/maintenance/search.php">Drink Search</a></li>
+            <li><a href="<?php echo $html_root?>/dishes_queries/dishes_page.php">Dishes search using the topbar</a></li>
         </ul>
         </div>
     </body>
