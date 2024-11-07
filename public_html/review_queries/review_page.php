756307ac6661282ddab1147f014f020bf455c144
tpdkrt <tpodko@gmail.com>
2024-11-07 17:13:59 +0100

    big fixed

--- public_html/review_queries/review_page.php
+++ public_html/review_queries/review_page.php
@@ -1,7 +1,6 @@
 <?php
         include '../navbar.php';
         global $our_root;
-        include $php_root . '/maintenance/security.php';
         include $php_root . '/logger.php';
     ?>
 <!DOCTYPE html>
