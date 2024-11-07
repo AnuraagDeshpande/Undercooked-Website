91a3756bfa0d9c64cd59adcfd6a3e9d0cd0d9be5
tpdkrt <tpodko@gmail.com>
2024-11-07 11:49:52 +0100

    starting work on graph scripting file

---
+++ logger_graph.py
@@ -0,0 +1,27 @@
+import matplotlib as plt
+import numpy as np
+
+file=open("log.txt")
+data=file.readlines()#opening the file
+title = "Title"
+plt.title(title)
+labels=["line 1", "line 2"]#labels for graphs and table
+colors=["purple", "pink"]
+i=0
+x=[]
+y=[]
+for line in data:#here we extract the actual data
+    user=line[line.find("user")+5:]
+    print(user)
+    
+    #x=np.linspace(1,len(row)+1, len(row))#create an arguement
+    #plt.plot(x,np.array(row), label=labels[i], color=colors[i])
+#we add some detials to the graph
+'''plt.xlabel("something")
+plt.ylabel("something")
+plt.grid()
+plt.legend()
+plt.savefig("graph.png")#save the file
+file.close()#close the file'''
+
+
