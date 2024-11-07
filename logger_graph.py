460aa1887936d89c8728e83b3fe2c68f4a4c0467
tpdkrt <tpodko@gmail.com>
2024-11-07 15:39:07 +0100

    python file improved

--- logger_graph.py
+++ logger_graph.py
@@ -6,22 +6,20 @@ data=file.readlines()#opening the file
 title = "Title"
 #plt.title(title)
 title = "Browser Usage Distribution"
-labels = ["Mozilla", "Apple", "Chrome", "Edge", "Else"]
-colors = ["purple", "pink", "skyblue", "lightgreen", "orange"]
+labels = ["Mozilla", "Apple", "Chrome", "Edge","Opera", "Else"]
+colors = ["purple", "blue", "lightgreen", "yellow", "orange","red"]
 
 # Initialize browser count dictionary
 browsers = dict.fromkeys(labels, 0)
 i=0
 for line in data:#here we extract the actual data
+    found=False
     for el in browsers:
-        browsers[el]+=line.__contains__(el)
-    '''user=line[line.find("user")+len("user")+3:]
-    user = user[:user.rfind("\"")]
-    y.append(user)
-    browser=line[line.find("browser")+len("browser")+3:]
-    browser = browser[:browser.rfind("\"")]
-    x.append(browser)
-    print(user)'''
+        if(line.__contains__(el)):
+            browsers[el]+=1
+            found=True
+    if(not found):
+       browsers["Else"]+=1
     
 #x=np.linspace(1,len(row)+1, len(row))#create an arguement
 #plt.scatter(np.array(x),np.array(y), label=labels[0], color=colors[0])
@@ -32,6 +30,7 @@ x = list(browsers.values())
 plt.figure(figsize=(8, 8))  # Set figure size
 plt.pie(x, labels=labels, colors=colors, autopct='%1.1f%%', startangle=140)
 plt.title(title)
+plt.legend()
 
 # Display and save the plot
 plt.savefig("graph.png")  # save the file
