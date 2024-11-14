bb6859970aa47882f7d2221602b485330877bd7a
Alen <akostov@constructor.university>
2024-11-14 12:48:41 +0100

    Some comments for testing

--- public_html/dishes_queries/navbar_for_dishes_search.php
+++ public_html/dishes_queries/navbar_for_dishes_search.php
@@ -1,6 +1,8 @@
 <?php
     //$our_root ='/~pyuri';
     //$php_root='/home/pyuri/repo/public_html'; 
+    //$php_root='C:\Users\kosto\Desktop\repo\public_html'; 
+    //$our_root ='/public_html'; 
     $our_root ='';
     $php_root='/home/tim/repo/public_html';
     include $php_root . '/maintenance/variables.php';
