64e133f1a750ab01f726ce071b72e573321ca200
Anuraag <andeshpand@constructor.university>
2024-11-20 17:56:15 +0100

    changed permissions, added a comment

--- public_html/Map/map_page.php
+++ public_html/Map/map_page.php
@@ -12,9 +12,9 @@
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
      integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
      crossorigin=""/> <!-- Leaflet CSS -->
-     <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+     <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"
      integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
-     crossorigin=""></script> <!-- Leaflet JavaScript file -->
+     crossorigin=""></script> <!-- Leaflet JavaScript file --> <!-- can change to 1.9.4 leaflet version -->
 </head>
 <body class = "secondary">
     <?php
